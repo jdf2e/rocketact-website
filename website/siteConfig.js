7937d70c97a2af1dea1ff52313acb40b4ce87a85
loveky <ylzcylx@gmail.com>
2018-12-26 08:59:06 +0800

    add scrollspy on TOC

--- website/siteConfig.js
+++ website/siteConfig.js
@@ -11,51 +11,52 @@
 // List of projects/orgs using your project for the users page.
 const users = [
   {
-    caption: 'User1',
+    caption: "User1",
     // You will need to prepend the image path with your baseUrl
     // if it is not '/', like: '/test-site/img/docusaurus.svg'.
-    image: '/img/docusaurus.svg',
-    infoLink: 'https://www.facebook.com',
-    pinned: true,
-  },
+    image: "/img/docusaurus.svg",
+    infoLink: "https://www.facebook.com",
+    pinned: true
+  }
 ];
 
-const repoUrl = 'https://github.com/jdf2e/rocketact';
+const repoUrl = "https://github.com/jdf2e/rocketact";
+const baseUrl = "/rocketact/";
 
 const siteConfig = {
-  title: 'Rocketact', // Title for your website.
-  tagline: '🚀 Developing React projects with ease',
-  url: 'https://jdf2e.github.io', // Your website URL
-  baseUrl: '/rocketact/', // Base URL for your project */
+  title: "Rocketact", // Title for your website.
+  tagline: "🚀 Developing React projects with ease",
+  url: "https://jdf2e.github.io", // Your website URL
+  baseUrl, // Base URL for your project */
   // For github.io type URLs, you would set the url and baseUrl like:
   //   url: 'https://facebook.github.io',
   //   baseUrl: '/test-site/',
 
   // Used for publishing and more
-  projectName: 'rocketact',
-  organizationName: 'jdf2e',
+  projectName: "rocketact",
+  organizationName: "jdf2e",
   // For top-level user or org sites, the organization is still the same.
   // e.g., for the https://JoelMarcey.github.io site, it would be set like...
   //   organizationName: 'JoelMarcey'
 
   // For no header links in the top nav bar -> headerLinks: [],
   headerLinks: [
-    {doc: 'getting-started', label: "Docs"},
-    {href: repoUrl, label: 'GitHub'},
+    { doc: "getting-started", label: "Docs" },
+    { href: repoUrl, label: "GitHub" }
   ],
 
   // If you have users set above, you add it here:
   users,
 
   /* path to images for header/footer */
-  headerIcon: 'img/docusaurus.svg',
-  footerIcon: 'img/docusaurus.svg',
-  favicon: 'img/favicon.png',
+  headerIcon: "img/docusaurus.svg",
+  footerIcon: "img/docusaurus.svg",
+  favicon: "img/favicon.png",
 
   /* Colors for website */
   colors: {
-    primaryColor: '#2E8555',
-    secondaryColor: '#205C3B',
+    primaryColor: "#2E8555",
+    secondaryColor: "#205C3B"
   },
 
   /* Custom fonts for website */
@@ -77,30 +78,30 @@ const siteConfig = {
 
   highlight: {
     // Highlight.js theme to use for syntax highlighting in code blocks.
-    theme: 'default',
+    theme: "monokai"
   },
 
   // Add custom scripts here that would be placed in <script> tags.
-  scripts: ['https://buttons.github.io/buttons.js'],
+  scripts: ["https://buttons.github.io/buttons.js", `${baseUrl}js/scrollspy.js`],
 
   // On page navigation for the current documentation page.
-  onPageNav: 'separate',
+  onPageNav: "separate",
   // No .html extensions for paths.
   cleanUrl: true,
 
   // Open Graph and Twitter card images.
-  ogImage: 'img/docusaurus.png',
-  twitterImage: 'img/docusaurus.png',
+  ogImage: "img/docusaurus.png",
+  twitterImage: "img/docusaurus.png",
 
   // Show documentation's last contributor's name.
-  // enableUpdateBy: true,
+  enableUpdateBy: true,
 
   // Show documentation's last update time.
-  // enableUpdateTime: true,
+  enableUpdateTime: true,
 
   // You may provide arbitrary config keys to be used as needed by your
   // template. For example, if you need your repo's URL...
-    repoUrl,
+  repoUrl
 };
 
 module.exports = siteConfig;
